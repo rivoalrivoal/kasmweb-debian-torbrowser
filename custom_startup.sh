8ee6bfc6972b973fd327512f200932c8bda3eaa6
Gurvan Rivoal <gurvan.rivoal@intrinsec.com>
2025-01-03 11:20:05 +0100

    --proxy-server in brave wrapper

--- custom_startup.sh
+++ custom_startup.sh
@@ -24,17 +24,6 @@ else
   exit 1
 fi
 
-# Replace __SOCKS_PROXY__ in the brave policy file
-file="/etc/brave/policies/managed/disable_tor.json"
-
-if [ -f "$file" ]; then
-  sed -i "s/__SOCKS_PROXY__/$socks_proxy/g" "$file"
-  echo "File updated: $file"
-else
-  echo "File not found: $file"
-  exit 1
-fi
-
 torbrowser-launcher
 
 tail -f /dev/null
